7743a225eacb2637e7b92a202fd939867b8b2027
someshtiwarii <someshtiwari96@gmail.com>
2021-07-25 11:34:40 +0530

    added styling

--- script.js
+++ script.js
@@ -1,31 +1,25 @@
-
-const Lucky=`<div class="label">Hurray!!You are a lucky person!</div> `
-const Unlucky=`<div class="label">Oops!!Your birthday is not a lucky number! </div>`
-
+const pass=`<div class="label">Yay!! Your birthday is a lucky number!</div>`
+const fail=`<div class="label">Oops!! Your birthday is not a lucky number!</div>`
 
 
-var birthDate = document.querySelector(".birthday");
-var inputNum = document.querySelector(".number");
-var inputBtn = document.querySelector(".btn");
-var outputDiv = document.querySelector(".output");
+const date = document.querySelector("#pickDate");
+const luckyNo = document.querySelector("#luckyNum");
+
+const outputDiv= document.querySelector(".output");
 
 
-
-inputBtn.addEventListener("submit", (e) => {
-   e.preventDefault();
-    
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const dateString = ((date.value).split('-')).join("");
     let sum=0;
-    const dateString = birthDate.toString().split("-");
-    
     for(letter of dateString){
      sum = sum + Number(letter);  
     }
-
-    const inputJS= Number(inputNum.value);
-    if(sum%inputJS === 0){
-        outputDiv.innerHTML = Lucky;
+    const luckyNoJS= Number(luckyNo.value);
+    if(sum%luckyNoJS ===0){
+        outputDiv.innerHTML = pass;
     }
     else{
-        outputDiv.innerHTML = Unlucky;
+        outputDiv.innerHTML = fail;
     }
 } );
