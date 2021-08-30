6b119b4761d91467fa57a97e812984b82da2d7cd
someshtiwarii <someshtiwari96@gmail.com>
2021-08-30 13:26:28 +0530

    added error handling

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-const pass=`<div class="label">Yay!! Your birthday is a lucky number!</div>`
-const fail=`<div class="label">Oops!! Your birthday is not a lucky number!</div>`
+const pass=`<div class="label">Yay!! Your birthday is a lucky number!</div>`;
+const fail=`<div class="label">Oops!! Your birthday is not a lucky number!</div>`;
+const errorMsg = `<div class="label">Enter both birthdate and lucky no. please!</div>`;
 
 
 const date = document.querySelector("#pickDate");
 const luckyNo = document.querySelector("#luckyNum");
-
+const checkBtn = document.querySelector(".check-button");
 const outputDiv= document.querySelector(".output");
 
 
-form.addEventListener('submit', (e)=>{
-    e.preventDefault();
+function errorHandler(){
+
+if(date.value && luckyNo.value){
     const dateString = ((date.value).split('-')).join("");
     let sum=0;
     for(letter of dateString){
@@ -22,4 +24,12 @@ form.addEventListener('submit', (e)=>{
     else{
         outputDiv.innerHTML = fail;
     }
-} );
+
+}
+else{
+    outputDiv.innerHTML= errorMsg;
+}
+};
+
+
+checkBtn.addEventListener("click",errorHandler);
