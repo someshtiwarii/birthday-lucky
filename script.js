babeb9c7c12971056d8392efd8c879478f74fd86
someshtiwarii <someshtiwari96@gmail.com>
2021-09-15 20:46:14 +0530

    error fixed

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const pass=`<div class="label">Yay!! Your birthday is a lucky number!</div>`;
 const fail=`<div class="label">Oops!! Your birthday is not a lucky number!</div>`;
-const errorMsg = `<div class="label">Enter both birthdate and lucky no. please!</div>`;
+const errorMsg = `<div class="label">Enter both birthdate and positive lucky no. please!</div>`;
 
 
 const date = document.querySelector("#pickDate");
@@ -11,7 +11,7 @@ const outputDiv= document.querySelector(".output");
 
 function errorHandler(){
 
-if(date.value && luckyNo.value){
+if(date.value && luckyNo.value>0){
     const dateString = ((date.value).split('-')).join("");
     let sum=0;
     for(letter of dateString){
